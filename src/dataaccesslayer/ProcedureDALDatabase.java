989bf573d30094467e60f578179f1d48147f2080
Francesco Avallone <f.avallone20@studenti.unisa.it>
2020-11-30 18:40:47 +0100

    Modification of get method of ProcedureDALDatabase and test class

--- src/dataaccesslayer/ProcedureDALDatabase.java
+++ src/dataaccesslayer/ProcedureDALDatabase.java
@@ -5,13 +5,16 @@
  */
 package dataaccesslayer;
 
+import businesslogiclayer.Competency;
 import businesslogiclayer.Procedure;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -23,6 +26,11 @@ public class ProcedureDALDatabase implements ProcedureDAL {
 
     private Connection conn;
 
+    /**
+     * Insert a specific procedure in the database 
+     * @param procedure
+     * @return the procedure inserted from the database
+     */
     @Override
     public Procedure insert(Procedure procedure) {
         boolean connectionWasClosed = DatabaseConnection.isClosed();
@@ -49,6 +57,11 @@ public class ProcedureDALDatabase implements ProcedureDAL {
         return null;
     }
 
+    /**
+     * Update a specific procedure in the database 
+     * @param procedure
+     * @return the procedure updated from the database
+     */
     @Override
     public Procedure update(Procedure procedure) {
         boolean connectionWasClosed = DatabaseConnection.isClosed();
@@ -77,6 +90,11 @@ public class ProcedureDALDatabase implements ProcedureDAL {
         return null;
     }
 
+    /**
+     * Delete a procedure with a specific id 
+     * @param id
+     * @return the procedure deleted from the database
+     */
     @Override
     public Procedure delete(int id) {
         boolean connectionWasClosed = DatabaseConnection.isClosed();
@@ -102,6 +120,10 @@ public class ProcedureDALDatabase implements ProcedureDAL {
         return null;
     }
 
+    /**
+     * Get all procedures from the database
+     * @return all procedures from the database
+     */
     @Override
     public List<Procedure> getAll() {
         boolean connectionWasClosed = DatabaseConnection.isClosed();
@@ -127,6 +149,11 @@ public class ProcedureDALDatabase implements ProcedureDAL {
         return null;
     }
 
+    /**
+     * Get a specific procedure from the database
+     * @param id
+     * @return the procedure specified
+     */
     @Override
     public Procedure get(int id) {
         boolean connectionWasClosed = DatabaseConnection.isClosed();
@@ -142,6 +169,20 @@ public class ProcedureDALDatabase implements ProcedureDAL {
                 dbProcedure = new Procedure(rs.getInt("id"), rs.getString("name"),
                         rs.getString("smp"));
             }
+            prepareStatement
+                    = conn.prepareStatement("select tb1.id as ProcedureId, tb1.name as ProcedureName, tb1.smp as ProcedureSmp,\n" +
+                    "competency.id as CompetencyId, competency.description as CompetencyDescription " +
+                    "from (procedure inner join procedurehascompetencies on " +
+                    "procedure.id = procedurehascompetencies.procedureid) as tb1 " +
+                    "inner join competency on tb1.competencyid = competency.id "+
+                    "where ProcedureId = ?; ");
+            prepareStatement.setInt(1, id);
+            rs = prepareStatement.executeQuery();
+            Competency competency = null;
+            while (rs.next()) {
+                competency = new Competency(rs.getInt("CompetencyId"), rs.getString("CompetencyDescription"));
+                dbProcedure.addCompetency(competency);
+            }
             if (connectionWasClosed) {
                 conn.close();
             }
@@ -152,6 +193,10 @@ public class ProcedureDALDatabase implements ProcedureDAL {
         return null;
     }
     
+    /**
+     * Delete all procedures from the database
+     * @return the procedures deleted from the database
+     */
     @Override
     public List<Procedure> deleteAll()
     {
