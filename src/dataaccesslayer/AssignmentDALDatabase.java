acee6aa601e2406a5787759d6a13e96a7949b283
Francesco Avallone <f.avallone20@studenti.unisa.it>
2020-12-01 09:20:34 +0100

    Implementation of AssignmentDALDatabase

--- src/dataaccesslayer/AssignmentDALDatabase.java
+++ src/dataaccesslayer/AssignmentDALDatabase.java
@@ -5,9 +5,13 @@
  */
 package dataaccesslayer;
 
+import businesslogiclayer.Activity;
 import businesslogiclayer.Assignment;
+import businesslogiclayer.Maintainer;
+import businesslogiclayer.User;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.HashSet;
 import java.util.Set;
@@ -20,13 +24,48 @@ import java.util.logging.Logger;
  */
 public class AssignmentDALDatabase implements AssignmentDAL {
 
-    private Connection conn; 
+    private Connection conn;
+
+    /**
+     * Get all Assignment with a specific week
+     * @param week
+     * @return 
+     */
     @Override
     public Set<Assignment> getAllForWeek(int week) {
         boolean connectionWasClosed = DatabaseConnection.isClosed();
         Set<Assignment> assignmentSet = new HashSet<>();
-       
+        ActivityDAL activityDAL = new ActivityDALDatabase();
+        UserDALDatabase userDAL = new UserDALDatabase();
+        try {
+            conn = DatabaseConnection.getConnection();
+            PreparedStatement prepareStatement
+                    = conn.prepareStatement("select idmaintainer, idactivity, day,hour from"
+                            + " assignment inner join activity on idactivity = activity.id"
+                            + " where week = ?; ");
+            prepareStatement.setInt(1, week);
+            ResultSet rs = prepareStatement.executeQuery();
+            Assignment assignment = null;
+            Activity activity = null;
+            User user = null;
+            Maintainer maintainer = null;
+            while (rs.next()) {
+                int idactivity = rs.getInt("idactivity");
+                activity = activityDAL.get(idactivity);
+                user = userDAL.get(rs.getInt("idmaintainer"));
+                maintainer = new Maintainer(user.getId(), user.getUsername(), user.getPassword());
+                assignment = new Assignment(maintainer, activity, rs.getString("day"), rs.getInt("hour"));
+                assignmentSet.add(assignment);
+            }
+            if (connectionWasClosed) {
+                conn.close();
+            }
+            return assignmentSet;
+        } catch (SQLException ex) {
+            Logger.getLogger(SiteDALDatabase.class.getName()).log(Level.SEVERE, null, ex);
+        }
         return null;
+
     }
-    
+
 }
