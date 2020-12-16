e11b571fdeb8f182cd4a97b6dcbb30d8d67dfe5f
Alessandro De Stefano <73339338+EarendilTiwele@users.noreply.github.com>
2020-12-16 11:49:15 +0100

    Final method getDAOFactory

--- src/dataaccesslayer/DAOFactory.java
+++ src/dataaccesslayer/DAOFactory.java
@@ -16,8 +16,8 @@ public abstract class DAOFactory {
     // List of DAO types supported by the factory
     public static final int POSTGRES = 1;
 
-    /* There will be a method for each DAO that can be 
-    created. The concrete factories will have to 
+    /* There will be a method for each DAO that can be
+    created. The concrete factories will have to
     implement these methods. */
     public abstract ActivityDAO getActivityDAO();
 
@@ -43,8 +43,7 @@ public abstract class DAOFactory {
      * @param whichFactory the type of DAO Factory to be returned: POSTGRES
      * @return the specific DAO Factory
      */
-    public static DAOFactory getDAOFactory(int whichFactory) {
-
+    public final static DAOFactory getDAOFactory(int whichFactory) {
         switch (whichFactory) {
             case POSTGRES:
                 return new PostgresDAOFactory();
